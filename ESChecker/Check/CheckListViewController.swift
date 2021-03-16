d74b3572f51397fcbfed65cbd69768c70a79c32b
Ryusei7070 <renshima7070@gmail.com>
2021-03-17 01:46:21 +0900

    Rysuei

---
+++ ESChecker/Check/CheckListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CheckListViewController.swift
+//  ESChecker
+//
+//  Created by Ryusei Hiraoka on 2021/03/17.
+//
+
+import UIKit
+
+class CheckListViewController: UIViewController {
+    
+    @IBOutlet weak var CheckTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+    @IBAction func addBtnAction(_ sender: Any){}
+}
